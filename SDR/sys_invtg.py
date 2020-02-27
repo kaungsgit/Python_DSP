bab37445d98d7b00e99d95abac85020abe19d360
kaungsgit <tycoonkg@gmail.com>
2020-02-27 01:00:27 -0500

    Added impulse response, freq response, and pole zero plots Handles both s and z domain - z domain default Fs is 1 - s domain fstop can be set through fstop_c

--- SDR/sys_invtg.py
+++ SDR/sys_invtg.py
@@ -13,18 +13,20 @@ import control as con
 import control.matlab as mctrl
 
 T = np.pi / 100  # sampling period
-print('Sampling rate is {}Hz'.format(1/T))
+Fs = 1 / T
+print('Sampling rate is {}Hz'.format(1 / T))
 z = con.tf('z')
 zm1 = 1 / z
-# sys_und_tst = z / (z - 1)
-sys_und_tst = 1 + 1 / z
+sys_und_tst = z / (z - 1)
+# sys_und_tst = 1 + 1 / z
 
-sys_und_tst = 1 + z ** -1 + z ** -2
+# sys_und_tst = 1 + z ** -1 + z ** -2
 
 s = con.tf('s')
 
-# sys_und_tst = 1/s
+# sys_und_tst = 1 / s
 # sys_und_tst = 1 / (s ** 3 + 2 * s ** 2 + 2 * s)
+fstop_c = 0.5
 
 # impulse response
 t_cd, im_resp = con.impulse_response(sys_und_tst)
@@ -37,29 +39,39 @@ plt.ylabel("Magnitude")
 plt.grid()
 
 # plot frequency response log scale
-w = 2 * np.pi * np.logspace(-3, .2, 100)
-mag, phase, w = con.freqresp(T * sys_und_tst, w)
+
+if sys_und_tst.isdtime():
+    w = 2 * np.pi * np.logspace(-3, np.log10(0.5), 100)
+else:
+    w = 2 * np.pi * np.logspace(-3, np.log10(fstop_c), 100)
+
+mag, phase, w = con.freqresp(sys_und_tst, w)
 
 # freq response returns mag and phase as [[[mag]]], [[[phase]]]
 # squeeze reduces this to a one dimensional array, optionally can use mag[0][0]
 mag = np.squeeze(mag)
 phase = np.squeeze(phase)
 plt.figure()
-plt.semilogx(w, hf.db(mag))
+plt.semilogx(w / (2 * np.pi), hf.db(mag))
 plt.grid()
 plt.xlabel('Frequency [Hz]')
 plt.ylabel('Magnitude [dB]')
 plt.title('Frequency Response')
 
 # plot frequency response linear scale
-w = 2 * np.pi * np.linspace(0, 1, 100)
-mag, phase, w = con.freqresp(T * sys_und_tst, w)
+
+if sys_und_tst.isdtime():
+    w = 2 * np.pi * np.linspace(0, 0.5, 100)
+else:
+    w = 2 * np.pi * np.linspace(0, fstop_c, 100)
+
+mag, phase, w = con.freqresp(sys_und_tst, w)
 # freq response returns mag and phase as [[[mag]]], [[[phase]]]
 # squeeze reduces this to a one dimensional array, optionally can use mag[0][0]
 mag = np.squeeze(mag)
 phase = np.squeeze(phase)
 plt.figure()
-plt.plot(w, hf.db(mag))
+plt.plot(w / (2 * np.pi), hf.db(mag))
 plt.grid()
 plt.xlabel('Frequency [Hz]')
 plt.ylabel('Magnitude [dB]')

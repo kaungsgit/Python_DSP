54113fbe8e1cd7b0be549af16d6829728696f121
kaungsgit <tycoonkg@gmail.com>
2020-03-18 19:11:48 -0400

    autocorr of two random signals added

--- Python_Mscs/random_vars.py
+++ Python_Mscs/random_vars.py
@@ -3,8 +3,9 @@
 
 from scipy import stats
 import matplotlib.pyplot as plt
-
+import scipy.signal as sig
 import numpy as np
+from numpy.random import randn
 
 # X = stats.binom(10, 0.2) # Declare X to be a binomial random variable
 # X = stats.norm()
@@ -16,21 +17,35 @@ import numpy as np
 # print(X.rvs())            # Get a random sample from X
 # print(X.rvs(10))          # Get 10 random samples form X
 
-no_realz = 10
+no_realz = 1
 realz_len = 100000
 
 x = np.zeros([no_realz, realz_len])
 for i in range(no_realz):
-    x[i, :] = np.random.randn(1, realz_len)
+    x[i, :] = randn(1, realz_len)
 
 # plt.figure()
 for i in range(no_realz):
     plt.subplot(no_realz, 1, i + 1)
     # plt.stem(x[i, :])
-    histVal, _, _ = plt.hist(x[i, :], bins=200)
+    histVal, _, _ = plt.hist(x[i, :], bins=200, density=True)
     plt.xlim(-5, 5)
     plt.ylim(0, max(histVal))
 
 print(sum(x[:, 2]) / len(x[:, 2]))
+
+# autocorrelation (correlation vs delay for same function)
+# noise signal
+N = 500
+lag = np.arange(-N + 1, N)
+noise = randn(N) + 1j * randn(N)
+
+corr = sig.correlate(noise, noise)
+
+plt.figure()
+plt.plot(lag, np.abs(corr))
+plt.xlabel("Lag [samples]")
+plt.ylabel("Real Magnitude")
+plt.title(f"AWGN Autocorrelation {N} Samples")
 
 plt.show()

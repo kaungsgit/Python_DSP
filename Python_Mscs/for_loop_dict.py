d571b7ad8c3d6093671b25465024df6d1d6cd61d
kaungsgit <tycoonkg@gmail.com>
2020-05-08 00:14:12 -0400

    Better design pattern for nested for loops - Uses cartesian product to get permutations of lists - Different ways to setting a sweep parameter are realized through polymorphism (one class for each sweep param) Note: The problem is now to find out how to pass data between the sweep param classes

--- Python_Mscs/for_loop_dict.py
+++ Python_Mscs/for_loop_dict.py
@@ -1,57 +1,72 @@
 import numpy as np
 from collections import OrderedDict
 
-loop_param = OrderedDict()
-loop_param['temps'] = [-10, 15]
-loop_param['Fs_list'] = [4000, 6000]
-loop_param['fins'] = [4500, 2234]
+# to compute all possible permutations
+# using itertools.product()
+import itertools
 
-curr_param = OrderedDict()
 
-b = np.zeros(len(loop_param))
-for i, val in enumerate(loop_param.values()):
-    b[i] = len(val)
-
-c = np.prod(b)
-
-print(f'Number of iteration is {c}')
-
-# for key, value in loop_param.items():
-#     print(key)
-#     print(value)
-#     for list_val in value:
-#         print(list_val)
-
-loop_param_keys = list(loop_param.keys())
-
-for temp in loop_param['temps']:
-
-    n = loop_param_keys.index('temps')
-    print(f'Current {loop_param_keys[n]} is {temp}')
-    curr_param[loop_param_keys[n]] = temp
-
-    for fs in loop_param['Fs_list']:
-
-        n = loop_param_keys.index('Fs_list')
-        print(f'Current {loop_param_keys[n]} is {fs}')
-        curr_param[loop_param_keys[n]] = fs
-
-        for fin in loop_param['fins']:
-
-            n = loop_param_keys.index('fins')
-            print(f'Current {loop_param_keys[n]} is {fin}')
-            curr_param[loop_param_keys[n]] = fin
-
-            # inside the innermost for loop
-            # make up string with all the curr param keys and values
-            name2 = ''
-            for key, value in curr_param.items():
-                name1 = key + str(value) + '_'
-                name2 = name2 + name1
-
-            print(name2)
-
-print('1')
+# # initializing list of list
+# all_list = [[1, 3], [6], [8, 10]]
+#
+# # printing lists
+# print("The original lists are : " + str(all_list))
+#
+# # using itertools.product()
+# # to compute all possible permutations
+# res = list(itertools.product(*all_list))
+#
+# loop_param = OrderedDict()
+# loop_param['temps'] = [-10, 15]
+# loop_param['Fs_list'] = [4000, 6000]
+# loop_param['fins'] = [4500, 2234]
+#
+# curr_param = OrderedDict()
+#
+# b = np.zeros(len(loop_param))
+# for i, val in enumerate(loop_param.values()):
+#     b[i] = len(val)
+#
+# c = np.prod(b)
+#
+# print(f'Number of iteration is {c}')
+#
+# # for key, value in loop_param.items():
+# #     print(key)
+# #     print(value)
+# #     for list_val in value:
+# #         print(list_val)
+#
+# loop_param_keys = list(loop_param.keys())
+#
+# for temp in loop_param['temps']:
+#
+#     n = loop_param_keys.index('temps')
+#     print(f'Current {loop_param_keys[n]} is {temp}')
+#     curr_param[loop_param_keys[n]] = temp
+#
+#     for fs in loop_param['Fs_list']:
+#
+#         n = loop_param_keys.index('Fs_list')
+#         print(f'Current {loop_param_keys[n]} is {fs}')
+#         curr_param[loop_param_keys[n]] = fs
+#
+#         for fin in loop_param['fins']:
+#
+#             n = loop_param_keys.index('fins')
+#             print(f'Current {loop_param_keys[n]} is {fin}')
+#             curr_param[loop_param_keys[n]] = fin
+#
+#             # inside the innermost for loop
+#             # make up string with all the curr param keys and values
+#             name2 = ''
+#             for key, value in curr_param.items():
+#                 name1 = key + str(value) + '_'
+#                 name2 = name2 + name1
+#
+#             print(name2)
+#
+# print('1')
 
 
 def for_loop(loop_dict_iter):
@@ -67,6 +82,9 @@ def for_loop(loop_dict_iter):
         except StopIteration:
             pass
 
+    return 0
+
+
 # for_loop(iter(loop_param.values()))
 #
 # mylist = iter(["apple", "banana", "cherry"])
@@ -78,3 +96,67 @@ def for_loop(loop_dict_iter):
 # print(x)
 # x = next(mylist, "orange")
 # print(x)
+
+class GenericParams:
+
+    def __init__(self, name='generic_param'):
+        self.name = name
+        self.value = -1000
+        self.some_generic_var = 0
+
+    def set_param(self, key=None, value=None):
+        print(f'Setting {key} to {value}...')
+        self.value = value
+
+
+class Temp(GenericParams):
+
+    def set_param(self, key=None, value=None):
+        print(f'Setting {key} to {value}...')
+        self.value = value
+        if value == -10:
+            self.some_generic_var = 13
+        else:
+            self.some_generic_var = 0
+
+
+class Fadc(GenericParams):
+
+    def set_param(self, key=None, value=None):
+        print(f'Setting {key} to {value}...')
+        self.value = value
+        if self.some_generic_var == 13:
+            print('doing some extra stuff')
+
+
+class Fin(GenericParams):
+
+    def set_param(self, key=None, value=None):
+        print(f'Setting {key} to {value}...')
+        self.value = value
+
+
+loop_param = OrderedDict()
+loop_param['Temp'] = [-10, 15]
+loop_param['Fadc'] = [4000, 6000]
+loop_param['Fin'] = [4500, 2234]
+
+keys, values = zip(*loop_param.items())
+iterations = [dict(zip(keys, v)) for v in itertools.product(*values)]
+
+for i in iterations:
+    print(i)
+
+    for param_key, param_value in i.items():
+        # print(f'param_key is {param_key} and param_value is {param_value}')
+
+        # print(f'temp is {i["Temp"]}')
+
+        curr_param_class = globals()[param_key]
+        curr_param_class_instance = curr_param_class(name=param_key)
+
+        curr_param_class_instance.set_param(param_key, param_value)
+
+    print('All params are set. Ready to collect data!!!')
+
+print('1')

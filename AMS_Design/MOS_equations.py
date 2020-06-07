2f6aeeb067d36880e2f2a9700fe47b902e5d9459
kaungsgit <tycoonkg@gmail.com>
2020-06-07 19:01:29 -0400

    Added absolute sign around VDS in PMOS square law calculations VDS_p must be negative, VDS_p < VGS_p - VTH_p

--- AMS_Design/MOS_equations.py
+++ AMS_Design/MOS_equations.py
@@ -1,29 +1,32 @@
 import numpy as np
 import sympy as sp
+from sympy import init_printing
 from engineering_notation import EngNumber as en
 
 ''' ************************************ calculate NMOS ID, VGS, or VDS ************************************'''
-
-# # Baker parameters
-Kp_n = 120e-6
-VTH_n = 0.8
-lmda_n = 0.01
+init_printing()
+# Baker parameters
+# Kp_n = 120e-6
+# VTH_n = 0.8
+# lmda_n = 0.01
 
 # # McNeill parameters
-# Kp_n = 26e-6
-# VTH_n = 1.9
-# lmda_n = 0.05
+Kp_n = 26e-6
+VTH_n = 1.9
+lmda_n = 0.05
 
 a = dict()
-a['VGS_n_val'] = '?'
+a['VGS_n_val'] = 2.5
 a['VDS_n_val'] = 2.5
-a['ID_n_val'] = 40e-6
+a['ID_n_val'] = '?'
 a['W_n'] = 10
 a['L_n'] = 2
 
 ID_n, VGS_n, VDS_n, W_n, L_n = sp.symbols('ID_n, VGS_n, VDS_n, W_n, L_n')
 
 h = Kp_n / 2 * W_n / L_n * (VGS_n - VTH_n) ** 2 * (1 + lmda_n * VDS_n) - ID_n
+
+# sp.display(h)
 
 x = [(ID_n, a['ID_n_val']), (VGS_n, a['VGS_n_val']), (VDS_n, a['VDS_n_val']), (W_n, a['W_n']), (L_n, a['L_n'])]
 
@@ -59,15 +62,16 @@ VTH_p = -1.6
 lmda_p = 0.028
 
 a = dict()
-a['VGS_p_val'] = '?'
-a['VDS_p_val'] = 2.5
-a['ID_p_val'] = 20e-6
-a['W_p'] = 900
-a['L_p'] = 10
-
+a['VGS_p_val'] = -2.5
+# VDS is negative, but there's absolute in equation, so doesn't matter what you put in
+# look at McNeill's notes Lecture 04 ECE4902
+a['VDS_p_val'] = -2.5
+a['ID_p_val'] = 26.32e-6
+a['W_p'] = '?'
+a['L_p'] = 2
 ID_p, VGS_p, VDS_p, W_p, L_p = sp.symbols('ID_p, VGS_p, VDS_p, W_p, L_p')
 
-h = Kp_p / 2 * W_p / L_p * (VGS_p - VTH_p) ** 2 * (1 + lmda_p * VDS_p) - ID_p
+h = Kp_p / 2 * W_p / L_p * (VGS_p - VTH_p) ** 2 * (1 + lmda_p * abs(VDS_p)) - ID_p
 
 x = [(ID_p, a['ID_p_val']), (VGS_p, a['VGS_p_val']), (VDS_p, a['VDS_p_val']), (W_p, a['W_p']), (L_p, a['L_p'])]
 

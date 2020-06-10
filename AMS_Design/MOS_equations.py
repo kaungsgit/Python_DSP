6f9f064f83c86434e8375fcf1c9d6792a725354c
kaungsgit <tycoonkg@gmail.com>
2020-06-10 03:27:24 -0400

    Added 5 precision places for printing usinig eng()

--- AMS_Design/MOS_equations.py
+++ AMS_Design/MOS_equations.py
@@ -3,9 +3,16 @@ import sympy as sp
 from sympy import init_printing
 from engineering_notation import EngNumber as en
 
+# saturation region square law calculator
+
 ''' ************************************ calculate NMOS ID, VGS, or VDS ************************************'''
 init_printing()
-# Baker parameters
+# # Baker parameters
+# Kp_n = 120e-6
+# VTH_n = 0.8
+# lmda_n = 0.01
+
+# # Baker parameters short channel 50n
 # Kp_n = 120e-6
 # VTH_n = 0.8
 # lmda_n = 0.01
@@ -16,11 +23,11 @@ VTH_n = 1.9
 lmda_n = 0.05
 
 a = dict()
-a['VGS_n_val'] = 2.5
-a['VDS_n_val'] = 2.5
-a['ID_n_val'] = '?'
-a['W_n'] = 10
-a['L_n'] = 2
+a['VGS_n_val'] = '?'
+a['VDS_n_val'] = 3.19
+a['ID_n_val'] = 10e-6
+a['W_n'] = 190
+a['L_n'] = 10
 
 ID_n, VGS_n, VDS_n, W_n, L_n = sp.symbols('ID_n, VGS_n, VDS_n, W_n, L_n')
 
@@ -38,16 +45,16 @@ for i in x:
 
 output_val_n = float(max(sp.solve(h.subs(x))))
 
-print(f'{calculated_para[0]} is {en(output_val_n)}')
+print(f'{calculated_para[0]} is {en(output_val_n, 5)}')
 
 for k, v in a.items():
     if v == '?':
         a[k] = output_val_n
 
 Rout_n = 1 / (lmda_n * a['ID_n_val'])
-print(f'Rout_n is {en(Rout_n)}')
+print(f'Rout_n is {en(Rout_n, 5)}')
 gm_n = 2 * a['ID_n_val'] / (a['VGS_n_val'] - VTH_n)
-print(f'gm is {en(gm_n)}\n')
+print(f'gm is {en(gm_n, 5)}\n')
 
 ''' ************************************ calculate PMOS ID, VGS, or VDS ************************************'''
 
@@ -61,14 +68,14 @@ Kp_p = 9.1e-6
 VTH_p = -1.6
 lmda_p = 0.028
 
-a = dict()
-a['VGS_p_val'] = -2.5
 # VDS is negative, but there's absolute in equation, so doesn't matter what you put in
 # look at McNeill's notes Lecture 04 ECE4902
+a = dict()
+a['VGS_p_val'] = '?'
 a['VDS_p_val'] = -2.5
-a['ID_p_val'] = 26.32e-6
-a['W_p'] = '?'
-a['L_p'] = 2
+a['ID_p_val'] = 19.9e-6
+a['W_p'] = 900
+a['L_p'] = 10
 ID_p, VGS_p, VDS_p, W_p, L_p = sp.symbols('ID_p, VGS_p, VDS_p, W_p, L_p')
 
 h = Kp_p / 2 * W_p / L_p * (VGS_p - VTH_p) ** 2 * (1 + lmda_p * abs(VDS_p)) - ID_p
@@ -83,13 +90,13 @@ for i in x:
 
 output_val_p = float(min(sp.solve(h.subs(x))))
 
-print(f'{calculated_para[0]} is {en(output_val_p)}')
+print(f'{calculated_para[0]} is {en(output_val_p, 5)}')
 
 for k, v in a.items():
     if v == '?':
         a[k] = output_val_p
 
 Rout_p = 1 / (lmda_p * a['ID_p_val'])
-print(f'Rout_p is {en(Rout_p)}')
+print(f'Rout_p is {en(Rout_p, 5)}')
 gm_p = 2 * a['ID_p_val'] / abs((a['VGS_p_val'] - VTH_p))
-print(f'gm is {en(gm_p)}\n')
+print(f'gm is {en(gm_p, 5)}\n')

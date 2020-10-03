8c03cc2420f83fa9a21376d6dfc2341d2eaa7daf
kaungsgit <tycoonkg@gmail.com>
2020-10-02 23:57:24 -0400

    Added responsePlot

--- custom_tools/handyfuncs.py
+++ custom_tools/handyfuncs.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def db(x):
@@ -6,3 +7,18 @@ def db(x):
     x = np.array(x)
     x_safe = np.where(x == 0, 1e-7, x)
     return 20 * np.log10(np.abs(x_safe))
+
+
+def responsePlot(w, h, title):
+    plt.subplot(2, 1, 1)
+    plt.semilogx(w / (2 * np.pi), 20 * np.log10(np.abs(h)))
+    plt.grid()
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Magnitude [dB]')
+    plt.title(title)
+    plt.subplot(2, 1, 2)
+    plt.semilogx(w / (2 * np.pi), np.unwrap(np.angle(h)) * 360 / (2 * np.pi))
+    plt.grid()
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Angle [deg]')
+    # plt.show()

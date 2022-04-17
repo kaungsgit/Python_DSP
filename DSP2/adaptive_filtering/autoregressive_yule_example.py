3d1417e659cbc8ec8e7a561eefb736149d7e6dce
kaungsgit <tycoonkg@gmail.com>
2022-04-17 02:05:03 -0400

    Performed cleanup.

--- DSP2/adaptive_filtering/autoregressive_yule_example.py
+++ DSP2/adaptive_filtering/autoregressive_yule_example.py
@@ -8,35 +8,52 @@ from numpy.random import randn
 from spectrum import aryule
 import pylab as pl
 
+# https://mpastell.com/pweave/_downloads/AR_yw.html (something similar)
+
 nsamps = 2 ** 12
 noise = 2 * randn(nsamps)
 
-b = [1]
-a = [1, 1, 1, 0.4]
-
+# ARMA is exactly like the generalized filter difference equation with both FIR and IIR
+b = [1]  # FIR portion
+a = [1, -2.2137, 2.9403, -2.1697, 0.9606]  # IIR portion
+# a = [1, 0]
 filtered_noise = sig.lfilter(b, a, noise)
+autocorr_noise = sig.correlate(noise, noise)
+autocorr_filtered_noise = sig.correlate(filtered_noise, filtered_noise)
 
-corr_sig = sig.correlate(filtered_noise, filtered_noise)
-
-corr_sig_0_end = corr_sig[filtered_noise.size - 1:]
+autocorr_sig_pos_only = autocorr_filtered_noise[filtered_noise.size - 1:]
 
 plt.figure()
-plt.plot(corr_sig_0_end)
+plt.plot(np.linspace(-noise.size + 1, noise.size - 1, (noise.size - 1) * 2 + 1),
+         autocorr_noise, label='autocorr of WGN')
+plt.xlabel('Lag')
+plt.ylabel('rnn power')
+plt.legend()
 
-corr_mat = toeplitz(corr_sig_0_end)
+plt.figure()
+plt.plot(autocorr_filtered_noise)
+plt.title('autocorr of filtered noise')
 
-corr_vec = np.append(corr_sig_0_end[1:], 0)
-
-a_vec = np.dot(np.linalg.inv(corr_mat), -corr_vec)
-a_vec_r = np.append(1, a_vec[:200])
+autocorr_mat = toeplitz(autocorr_sig_pos_only)
+# autocorr_vec starts from rxx[1] to rxx[M], which is zero-padded
+# since rxx[M] doesn't exist from original autocorrelation
+autocorr_vec = np.append(autocorr_sig_pos_only[1:], 0)
+# https://en.wikipedia.org/wiki/Computational_complexity_of_mathematical_operations#:~:text=a%20quantum%20computer-,Matrix%20algebra,-%5Bedit%5D
+# https://www.geeksforgeeks.org/analysis-algorithms-big-o-analysis/
+# inverting a 2**12 x 2**12 matrix is very taxing, O(n^3)
+a_est = np.dot(np.linalg.inv(autocorr_mat), -autocorr_vec)
+a_est_w_a0 = np.append(1, a_est[:20])
 
 # y = sig.lfilter([1], a, pl.randn(1, nsamps))
+# inverting big toeplitz matrix can be done with some smart algo
+# https://www.youtube.com/watch?v=AOX1ifbRfBU&ab_channel=AdamKashlak
 ar, variance, coeff_reflection = aryule(filtered_noise, 20)
 
 plt.figure()
 plt.plot(a, label='original coeffs')
-plt.plot(a_vec_r, label='estimated coeffs, manual')
+plt.plot(a_est_w_a0, label='estimated coeffs, manual, inverting full size toeplitz')
 plt.plot(np.append(1, ar), label='estimated coeffs, aryule')
+plt.title('AR Coeffs Comparison')
 plt.legend()
 plt.show()
 
